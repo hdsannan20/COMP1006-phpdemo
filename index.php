6ffe128e8bcc45891012926dd30d25f4bbbbc33f
harmandeep singh <harmandeep singh>
2017-02-06 15:39:02 -0500

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+$myName = "Tom";
+
+
+
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title><?php echo $myName ?></title>
+  </head>
+  <body>
+    <h1>Hello World!</h1>
+
+    <h2>Name: <?php echo $myName ?></h2>
+  </body>
+</html>
